05e36bb6ec46305168a6cbf3fe6d9af60a3194a1
= <=>
2026-01-22 11:06:51 +0530

    d

--- src/Components/ArrayNum.jsx
+++ src/Components/ArrayNum.jsx
@@ -2,7 +2,9 @@ import React from "react";
 const scores = [10, 50, 90, 30, 100];
 const ArrayNum = () => {
   const highScores = scores.filter((score) => score > 50);
-  return <div>{highScores.join(", ")}</div>;
+  return (
+  <div>{highScores.join(", ")}</div>
+  );
 };
 
 export default ArrayNum;
